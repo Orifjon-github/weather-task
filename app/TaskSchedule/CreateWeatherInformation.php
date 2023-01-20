88a7f6c5431f51e8e2528737fbe9fe671b6e1351
Orifjon94 <avazbekoripov94@gmail.com>
2023-01-20 19:23:33 +0500

    Create Hourly Schedule

---
+++ app/TaskSchedule/CreateWeatherInformation.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\TaskSchedule;
+
+use App\Models\City;
+use App\Models\WeatherInformation;
+
+class CreateWeatherInformation
+{
+    public function __invoke() {
+        $httpClient = new \GuzzleHttp\Client();
+        $cities = City::all();
+
+        foreach ($cities as $city) {
+            $lon = $city->longitude;
+            $lat = $city->latitude;
+            $request = $httpClient->get("https://api.openweathermap.org/data/2.5/weather?lat={$lat}&lon={$lon}2&appid=".env('API_KEY')."&units=metric");
+            $body = json_decode($request->getBody()->getContents());
+            WeatherInformation::create([
+                'city_id' => $city->id,
+                'time' => 'time',
+                'weather_name' => $body->weather[0]->main,
+                'longitude' => $lon,
+                'latitude' => $lat,
+                'temperature' =>  $body->main->temp,
+                'pressure' => $body->main->pressure,
+                'humidity' => $body->main->humidity,
+                'min_temperature' => $body->main->temp_min,
+                'max_temperature' => $body->main->temp_max,
+            ]);
+        }
+    }
+}
